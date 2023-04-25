8dbd1e761afe131608b55b6a6ab20c56ba576af1
lauArciniega2797 <arciniega1497@hotmail.com>
2023-04-25 17:46:10 -0600

    Base de datos en la nube, modificacion de la conexión PDO

--- portafolio/assets/config/db.php
+++ portafolio/assets/config/db.php
@@ -1,6 +1,7 @@
 <?php
     try {
-        $conn = new PDO('mysql:host=localhost;dbname=laura_jobs;charset=utf8', 'root', '');
+        // $conn = new PDO('mysql:host=localhost;dbname=laura_jobs;charset=utf8', 'root', '');
+        $conn = new PDO('mysql:host=sql9.freemysqlhosting.net;dbname=sql9614248;charset=utf8', 'sql9614248', 'aINJ1KAzyJ');
     } catch (PDOException $e) {
         echo "No se pudo conectar a la base de datos: " . $e;
     }
